d76c578a42fc9e8c855d4d48f9274b02f89b3aef
Gana498 <62788810+Gana498@users.noreply.github.com>
2025-07-20 17:02:00 +0000

    "Added Spring Security dependencies and SecurityConfig to secure all pages except h2-console."

---
+++ src/main/java/com/company/lms/cloud1/configurations/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.company.lms.cloud1.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;		
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+        .authorizeHttpRequests(auth -> auth
+        .requestMatchers("h2-console/**").permitAll()
+        .requestMatchers("/adminhome").hasRole("ADMIN")
+        .requestMatchers("/manageusers").hasRole("ADMIN")
+        .anyRequest().authenticated())
+        .logout(logout -> logout.permitAll())
+        .csrf(csrf -> csrf
+        .ignoringRequestMatchers("/h2-console/**"))
+        .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+}
+	
